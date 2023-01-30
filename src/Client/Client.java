78a56d5d3e0d752bcfd2cf62eb994986c38b1dcc
D.J Amarasinghe <maxwon555@gmail.com>
2023-01-30 22:27:02 +0530

    create Client.java file and handle all exceptions

---
+++ src/Client/Client.java
@@ -0,0 +1,39 @@
+package Client;
+
+import Server.Service;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+
+public class Client{
+
+    public static void main()
+    {
+        if(System.getSecurityManager() == null)
+        {
+            System.setSecurityManager(new RMISecurityManager());
+        }
+
+        Service service = null;
+
+        try{
+            service = (Service)Naming.lookup("//localhost/Cal_Service");
+
+            System.out.println("Add : "+ service.add(2, 2));
+        }
+        catch (MalformedURLException e)
+        {
+            throw new RuntimeException(e);
+        }
+        catch (NotBoundException e)
+        {
+            throw new RuntimeException(e);
+        }
+        catch (RemoteException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
