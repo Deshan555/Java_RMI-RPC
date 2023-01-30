b066ff9d6c8d5d81d63c75e51f45259dfcdb01b0
D.J Amarasinghe <maxwon555@gmail.com>
2023-01-30 20:35:04 +0530

    finalize Service.java file and define all functions in the Service.java file

--- src/Server/Service.java
+++ src/Server/Service.java
@@ -1,4 +1,17 @@
 package Server;
 
-public class Service {
+import java.rmi.Remote;
+
+import java.rmi.RemoteException;
+
+public interface Service extends Remote
+{
+    public int add(int a, int b) throws RemoteException;
+
+    public int multiply(int a, int b) throws RemoteException;
+
+    public int divide(int a, int b) throws RemoteException;
+
+    public int subtract(int a, int b) throws RemoteException;
+
 }
